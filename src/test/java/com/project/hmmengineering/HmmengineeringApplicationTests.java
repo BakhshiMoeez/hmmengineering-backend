a896cf7c3644f976a85f3b7d1a383eb82c3b5834
BakhshiMoeez <moeezbakhshi123456@gmail.com>
2024-02-24 23:34:14 +0500

    first commit

---
+++ src/test/java/com/project/hmmengineering/HmmengineeringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hmmengineering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmmengineeringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
